5753e05e05c56e6524c85c7318d4d99f8e6bcdd0
mashino <c0b2213326@edu.teu.ac.jp>
2023-09-26 15:51:16 +0900

    ああ

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01-20230926/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex01-20230926/fig/3.png")
+    kk_img = pg.transform.flip(kk_img,True,False)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img,[300,300])
+
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
